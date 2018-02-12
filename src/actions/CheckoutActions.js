f1d43a50375143b80c10d90a38d3a37c4912b1cb
Camden Phalen <camdenphalen@gmail.com>
2018-02-12 14:28:11 -0500

    checkout page revisions

--- src/actions/CheckoutActions.js
+++ src/actions/CheckoutActions.js
@@ -1,20 +1,16 @@
 import ActionTypes from './ActionTypes';
 
 export const nextCheckoutSection = () => {
-  return dispatch => {
-    dispatch({
-      type: ActionTypes.NEXT_CHECKOUT_SECTION,
-    });
+  return {
+    type: ActionTypes.NEXT_CHECKOUT_SECTION,
   };
 };
 
 export const setCheckoutState = checkoutState => {
-  return dispatch => {
-    dispatch({
-      type: ActionTypes.SET_CHECKOUT_STATE,
-      payload: {
-        checkoutState: checkoutState,
-      },
-    });
+  return {
+    type: ActionTypes.SET_CHECKOUT_STATE,
+    payload: {
+      checkoutState: checkoutState,
+    },
   };
 };
