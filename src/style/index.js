e708ef615b391ecb8bef8199c2a8a95eb7633820
aem <amarkon895@gmail.com>
2018-02-15 22:39:22 -0500

    allow named imports from style dir

---
+++ src/style/index.js
@@ -0,0 +1,11 @@
+import H1 from './H1';
+import H2 from './H2';
+import H3 from './H3';
+import H4 from './H4';
+import H6 from './H6';
+import Input from './Input';
+import Label from './Label';
+import P from './P';
+import Select from './Select';
+
+export { H1, H2, H3, H4, H6, Input, Label, P, Select };
