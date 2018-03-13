08997b5bfb1c9d8b1828f433c174222ac51bbefb
Camden Phalen <camdenphalen@gmail.com>
2018-03-12 21:48:42 -0400

    api wrappers

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,21 @@
-export const formatResponse = response => {
+const BASE_URL = 'https://shielded-cove-82777.herokuapp.com';
+
+const formatResponse = response => {
   return response.json().then(data => {
     return { data, status: response.status };
   });
+};
+
+export const get = endpoint => {
+  return fetch(`${BASE_URL}${endpoint}`, { method: 'get' }).then(
+    formatResponse
+  );
+};
+
+export const post = (endpoint, body) => {
+  return fetch(`${BASE_URL}${endpoint}`, {
+    body,
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+  }).then(formatResponse);
 };
