c8edc73df8db17b7f7af69eedb370a6036a6477b
VincentCarlino <vjcarlino22@gmail.com>
2018-03-19 21:09:08 -0400

    Implement blog page

--- src/components/blog/BlogInfo.js
+++ src/components/blog/BlogInfo.js
@@ -4,6 +4,7 @@ import { bindActionCreators } from 'redux';
 import PrismicPage from '../../prismic/PrismicPage';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
+import { RichText } from 'prismic-reactjs';
 import {
   H1,
   H2,
@@ -20,27 +21,31 @@ import {
 
 const Column = styled.div`
   grid-column: span 12;
-  height: 500px;
 `;
 
-function BlogInfo(props) {
-  console.log(props);
+class BlogInfo extends Component {
+  static pageType = 'blogpost';
 
-  return (
-    <Container>
-      <NavMargin>
-        <GridParent>
-          <Column>
-            <H1>This is a blog post</H1>
-            <H2>The main image for this post goes here</H2>
-            <H2>The title of the image will go here</H2>
-            <H4>The author and date will go here</H4>
-            <P>The main content of the post will go here</P>
-          </Column>
-        </GridParent>
-      </NavMargin>
-    </Container>
-  );
+  render() {
+    console.log(this.props);
+    return (
+      <Container>
+        <NavMargin>
+          <GridParent>
+            <Column>
+              <H1>{RichText.asText(this.props.doc.data.blog_title)}</H1>
+              <H4>
+                {this.props.doc.data.blog_date} -{' '}
+                {RichText.asText(this.props.doc.data.blog_author)}
+              </H4>
+              <Img src={this.props.doc.data.blog_image.url} />
+              <P>{RichText.render(this.props.doc.data.blog_content)}</P>
+            </Column>
+          </GridParent>
+        </NavMargin>
+      </Container>
+    );
+  }
 }
 
 export default PrismicPage(BlogInfo);
--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -54,7 +54,6 @@ const Routes = props => (
       exact
       path="/blog/:uid"
       routerProps={props}
-      componentProps={{ uid: 'bloginfo' }}
       component={BlogInfo}
     />
     <PrismicRoute
