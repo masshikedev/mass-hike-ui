565fb1b024019da07f880d975d63340c018c1356
Camden Phalen <camdenphalen@gmail.com>
2018-03-26 17:01:40 -0400

    import proxima nova

--- src/style/H4.js
+++ src/style/H4.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 //Requires proxima nova bold
 const H4 = styled.h4`
+  font-family: 'proxima-nova';
   font-weight: bold;
+  font-weight: 700;
 `;
 
 export default H4;
--- src/style/H5.js
+++ src/style/H5.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 //Requires proxima nova extrabold
 const H5 = styled.h5`
   font-size: 28px;
+  font-family: 'proxima-nova';
+  font-weight: 800;
 `;
 
 export default H5;
