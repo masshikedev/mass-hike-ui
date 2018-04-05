a81c806620a1f83ebf37b0ec1a29f46f7a291b9f
Vincent Carlino <vjcarlino22@gmail.com>
2018-04-05 12:31:23 -0400

    finally figured out timeline graphic

--- src/components/DetailDescription.js
+++ src/components/DetailDescription.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { H2, H6, P, MediaQueries } from '../style';
+import { H2, H6, P, constants, MediaQueries } from '../style';
 import { format } from 'date-fns';
 import { TIME } from '../utils/dateFormats';
 
@@ -14,6 +14,44 @@ const Wrapper = styled.div`
   }
 `;
 
+const Timeline = styled.div`
+  display: flex;
+  text-align: center;
+  padding: 40px 0;
+`;
+
+const SmallTime = styled.div`
+  flex: 1;
+`;
+
+const LargeTime = styled.div`
+  flex: 3;
+`;
+
+const Graphic = styled.div`
+  background-color: ${({ primary }) => (primary ? '#F6BD69' : '#BDCFA8')};
+  width: 80%;
+  height: 20px;
+  margin: 0 auto;
+  border-radius: 20px;
+`;
+
+const Line = styled.div`
+  height: 20px;
+  width 100%;
+
+  :after {
+    position: relative;
+    left: 0;
+    top: -50%;
+    height: 1px;
+    background: #000;
+    content: "";
+    width: 100%;
+    display: block;
+  }
+`;
+
 function DetailDescription(props) {
   const { detail, time } = props;
   return (
@@ -22,18 +60,38 @@ function DetailDescription(props) {
         What you need to know
       </P>
       <P>{detail.body}</P>
-      <H6>pickup</H6>
-      <P>{`${format(time.pickupStart, TIME)} - ${format(
-        time.pickupEnd,
-        TIME
-      )}`}</P>
-      <H6>hike time</H6>
-      <P>{`${format(time.hikeStart, TIME)} - ${format(time.hikeEnd, TIME)}`}</P>
-      <H6>dropoff</H6>
-      <P>{`${format(time.dropoffStart, TIME)} - ${format(
-        time.dropoffEnd,
-        TIME
-      )}`}</P>
+      <Timeline>
+        <SmallTime>
+          <H6>pickup</H6>
+          <Line>
+            <Graphic />
+          </Line>
+          <P>{`${format(time.pickupStart, TIME)} - ${format(
+            time.pickupEnd,
+            TIME
+          )}`}</P>
+        </SmallTime>
+        <LargeTime>
+          <H6>hike time</H6>
+          <Line>
+            <Graphic primary />
+          </Line>
+          <P>{`${format(time.hikeStart, TIME)} - ${format(
+            time.hikeEnd,
+            TIME
+          )}`}</P>
+        </LargeTime>
+        <SmallTime>
+          <H6>dropoff</H6>
+          <Line>
+            <Graphic />
+          </Line>
+          <P>{`${format(time.dropoffStart, TIME)} - ${format(
+            time.dropoffEnd,
+            TIME
+          )}`}</P>
+        </SmallTime>
+      </Timeline>
     </Wrapper>
   );
 }
