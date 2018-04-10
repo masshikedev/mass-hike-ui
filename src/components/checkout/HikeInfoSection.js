0deba8994d1cd02c877b8f5b5681cc2169cf430b
Matt Panzer <panzer.matthew@gmail.com>
2018-04-10 18:41:31 -0400

    rebasing, fixing edit buttons, font fixing

--- src/components/checkout/HikeInfoSection.js
+++ src/components/checkout/HikeInfoSection.js
@@ -46,6 +46,10 @@ const CheckBoxWrapper = styled.div`
   }
 `;
 
+const Caption = P.extend`
+  max-width: 500px;
+`;
+
 class HikeInfoSection extends BaseCheckoutSection {
   constructor(props) {
     super(props);
