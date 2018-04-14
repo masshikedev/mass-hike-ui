0961d48b555efbe6746f88a74959c4d3f1a2daa0
Matt Panzer <panzer.matthew@gmail.com>
2018-04-14 15:47:48 -0400

    kids tickets, added arrow image

--- src/utils/validationConstraints.js
+++ src/utils/validationConstraints.js
@@ -48,6 +48,18 @@ const hikeConstraints = trip => {
         notLessThanOrEqualTo: `^We have ${remaining} tickets remaining for this trip. Please enter a number up to ${remaining}`,
         onlyInteger: true,
         notInteger: '^Must be a whole number (1, 2, 3...)',
+        message: '^Please enter a number (1, 2, 3...)',
+      },
+    },
+    kids: {
+      presence: {
+        allowEmpty: false,
+        message: '^Please enter an amount',
+      },
+      numericality: {
+        onlyInteger: true,
+        notInteger: '^Must be a whole number (1, 2, 3...)',
+        message: '^Please enter a number (1, 2, 3...)',
       },
     },
     kids: {
