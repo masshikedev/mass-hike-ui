3da5798da87b1b9b077ded192e01188c2c5b1e7e
Matt Panzer <panzer.matthew@gmail.com>
2018-04-14 15:47:48 -0400

    kids tickets, added arrow image

--- src/style/Input.js
+++ src/style/Input.js
@@ -4,13 +4,15 @@ import constants from './constants';
 const Input = styled.input`
   box-sizing: border-box;
   font-size: 20px;
-  padding: 5px;
+  padding: 5px 10px;
   &[type='text'] {
     width: 100%;
     border-radius: 4px;
     background-color: #ffffff;
     border: solid 2px ${props => (props.invalid ? constants.red : 'black')};
-    max-width: 500px;
+    max-width: ${props =>
+      props.short ? '125px' : props.medium ? '250px' : '500px'};
+    margin-right: ${props => (props.short ? '15px' : '0px')};
   }
   &[type='password'] {
     width: 100%;
