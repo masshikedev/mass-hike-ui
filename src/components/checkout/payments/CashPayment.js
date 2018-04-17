c30fd8d8840709a13f0feffc1521fdef7ecb3190
Matt Panzer <panzer.matthew@gmail.com>
2018-04-17 01:15:35 -0400

    setting up and styling calendar

--- src/components/checkout/payments/CashPayment.js
+++ src/components/checkout/payments/CashPayment.js
@@ -152,6 +152,13 @@ class CashPayment extends BaseCheckoutSection {
     }
   }
 
+  availableDays() {
+    const { trip } = this.props;
+    return trip.cashAvailability.map(
+      dayData => new Date(dayData.date + DAY_PICKER_DATE_CORRECTION)
+    );
+  }
+
   renderCashLocations(maxLoc) {
     const { trip } = this.props;
     const cashLocations = trip.cashLocations;
@@ -185,8 +192,7 @@ class CashPayment extends BaseCheckoutSection {
   }
 
   renderCalendar() {
-    const { trip } = this.props;
-    const { meetingDate } = this.state;
+    const { meetingDate, trip } = this.props;
     return (
       <div>
         <Helmet>
