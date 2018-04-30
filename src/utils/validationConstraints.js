8d34be7c75f1ef5f0e3c01fb7c1938dd0cf1ed0d
Matt Panzer <panzer.matthew@gmail.com>
2018-04-29 22:19:49 -0400

    dup validation contraint for kids removed

--- src/utils/validationConstraints.js
+++ src/utils/validationConstraints.js
@@ -61,17 +61,6 @@ const hikeConstraints = trip => {
         message: '^Please enter a number (1, 2, 3...)',
       },
     },
-    kids: {
-      presence: {
-        allowEmpty: false,
-        message: '^Please enter an amount',
-      },
-      numericality: {
-        onlyInteger: true,
-        notInteger: '^Must be a whole number (1, 2, 3...)',
-        message: '^Please enter a number (1, 2, 3...)',
-      },
-    },
     pickupLocation: {
       presence: {
         allowEmpty: false,
